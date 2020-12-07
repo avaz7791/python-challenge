b3ed83e9207c3b61350ea1cfee1f66db8ca198a5
Amir Vazquez <j.amir.vazquez@gmai.com>
2020-12-06 22:29:56 -0800

    Poll checks

--- Python Challenge/PyPoll/main.py
+++ Python Challenge/PyPoll/main.py
@@ -9,3 +9,63 @@ Description: Process csv file election_data.csv to calculate the following:
     - The total number of votes each candidate won
     - The winner of the election based on popular vote.
 """
+
+#add libraries
+import os, csv
+
+#initialize variable
+csv_file = 'C:\\Users\\sonof\\UCSDProjects\\python-challenge\\Python Challenge\\PyPoll\\Resources\\election_data.csv'
+
+tot_votes = []
+Candidates = []
+
+with open(csv_file) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first
+    csv_header = next(csvreader)
+
+    # for each row of data after the header
+    for row in csvreader:
+       
+        tot_votes.append(row[0])
+        Candidates.append(row[2])
+x=0        
+CanditList =[]
+
+
+DistinctCandiadates = [CanditList.append(x) for x in Candidates if x not in CanditList ]
+
+print (len(CanditList))
+print (CanditList)
+
+_dict = {'Candidate','Votes'}
+
+_Candidates = []
+_Candidates.append(_dict)
+_Candidates.append('Jerry',20)
+
+print(_Candidates)
+
+    #for each month added to total_profit calculate the change in the month
+    #for plm in range(len(tot_profit) - 1):
+    #    PL_Change.append(tot_profit[ plm + 1]  - tot_profit[ plm ])
+
+
+# print(f'Election Results')
+# print(f'-------------------')
+# print(f'Total Votes: {len(tot_votes)}') # print the total number of months
+
+
+
+
+#output_txt = 'C:\\Users\\sonof\\UCSDProjects\\python-challenge\\Python Challenge\\PyPoll\\analysis\\PyPoll_Analysis_Summary.txt'
+
+#Print File
+# with open(output_txt,"w") as output:
+
+#     output.write(f'Election Results \n')
+#     output.write(f'------------------- \n')
+#     output.write(f'Total Votes: {len(tot_votes)} \n') # print the total number of months
